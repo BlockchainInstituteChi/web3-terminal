7d8ec349cb6d29d070e3616bd7a2a21cd0a76282
alexander-morris <alex@theblockchaininstitute.org>
2019-09-06 16:15:00 -0500

    working build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Terminal from 'react-bash';
+import sha256 from 'sha256';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const extensions = {
+    sudo: {
+        exec: ({ structure, history, cwd }) => {
+            return { structure, cwd,
+                history: history.concat({ value: 'Nice try... (ಠ(ಠ(ಠ_ಠ)ಠ)ಠ)' }),
+            };
+        },
+    },
+    hash : {
+        exec: (state, { flags, args }) => {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+            var response = "";
+
+            if ( args[0] ) {
+                response = sha256(args[0]);
+            } else {
+                response = "Please pass a string after the hash command. i.e. 'hash myTestString'";
+            }
+
+            return Object.assign({}, state, {
+                history: state.history.concat(
+                    { value: response }
+                ),
+            });
+        },
+    }
+};
+
+const history = [
+    { value: 'Welcome to Web3 Terminal' },
+    { value: 'Try out your commands here safely and become an expert in no time.' },
+    { value: 'Powered by The Blockchain Institute - Visit https://weteachblockchain.org/ for more great tools!' },
+    { value: 'Type `help` to begin' },
+];
+
+const structure = {
+    '.hidden': {
+        file1: { content: 'The is the content for file1 in the <.hidden> directory.' },
+        file2: { content: 'The is the content for file2 in the <.hidden> directory.' },
+        dir2: {
+            file: { content: 'The is the content for <file> in the <.hidden> directory.' },
+        },
+        '.secrets': { content: 'I\'m still afraid of the dark...' },
+    },
+    public: {
+        file1: { content: 'The is the content for file1 in the <public> directory.' },
+        file2: { content: 'The is the content for file2 in the <public> directory.' },
+        file3: { content: 'The is the content for file3 in the <public> directory.' },
+    },
+    'README.md': { content: '✌⊂(✰‿✰)つ✌ Thanks for checking out the tool! There is a lot that you can do with react-bash and I\'m excited to see all of the fun commands and projects build on top of it!' },
+};
+
+const Root = <Terminal history={history} structure={structure} extensions={extensions} />;
+ReactDOM.render(Root, document.getElementById('app'));
